a659087811427e0bd6c89f7b3b0752d17f020378
Andre Scheir Johansson <41080829+Scheir@users.noreply.github.com>
2023-03-23 13:10:22 +0100

    Update plugins.lua

--- neovimsetup/lua/plugins.lua
+++ neovimsetup/lua/plugins.lua
@@ -9,10 +9,17 @@ return require('packer').startup(function(use)
   use 'jackguo380/vim-lsp-cxx-highlight'
   use 'mhartington/oceanic-next'
   use { 'feline-nvim/feline.nvim' }
-  use {'lewis6991/gitsigns.nvim'}
+ --use {'lewis6991/gitsigns.nvim'}
   use {
           'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-           requires = { {'nvim-lua/plenary.nvim'} }
+            -- or                            , branch = '0.1.x',
+           requires = {
+               {'nvim-lua/plenary.nvim'},
+               { "nvim-telescope/telescope-live-grep-args.nvim" },
+            },
+            config = function()
+                require("telescope").load_extension("live_grep_args")
+                end
       }
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
   end)
